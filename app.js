edbb7757381eec410920c5600fc2e85c3c8c2814
vish9151 <84173784+vishal9151@users.noreply.github.com>
2023-10-28 13:23:13 +0530

    updated

---
+++ app.js
@@ -0,0 +1,62 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import { connectPassport } from "./utils/provider.js";
+import session from "express-session";
+import passport from "passport";
+import cookieParser from "cookie-parser";
+import { errorMiddleware } from "./middlewares/errorMiddlewares.js";
+
+import cors from "cors";
+
+const app = express();
+
+dotenv.config({
+  path: "./config/config.env",
+});
+
+// Using middlewares
+console.log(process.env.SECRET_ID);
+
+app.use(
+  session({
+    secret: process.env.SECRET_ID,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure:   process.env.NODE_ENV === "development" ? false : true,
+      httpOnly: process.env.NODE_ENV === "development" ? false : true,
+      sameSite: process.env.NODE_ENV === "development" ? false : "none",
+    },
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Initialize Passport and configure
+app.use(passport.authenticate("session"));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
+import userRoute from "./routes/user.js";
+import orderRoute from "./routes/orders.js";
+
+// Initialize Passport and configure it
+connectPassport();
+
+app.use("/api/v1", userRoute);
+app.use("/api/v1", orderRoute);
+
+app.use(errorMiddleware);
+
+export default app;
---
+++ utils/provider.js
@@ -0,0 +1,49 @@
+
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import passport from "passport";
+import { User } from "../models/User.js";
+
+export const connectPassport = () => {
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        callbackURL: process.env.CALLBACK_URI,
+      },
+      async function (accessToken, refreshToken, profile, done) {
+        console.log("inside callback function");
+        const user = await User.findOne({
+          googleId: profile.id,
+        });
+
+        if (!user) {
+          const newUser = await User.create({
+            googleId: profile.id,
+            name: profile.displayName,
+            photo: profile.photos[0].value,
+          });
+
+          return done(null, newUser);
+        } else {
+          console.log(user);
+          return done(null, user);
+        }
+      }
+    )
+  );
+
+  passport.serializeUser((user, done) => {
+	console.log("inside serialize function");
+    console.log(user.id);
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(async (id, done) => {
+	console.log("inside Deserialize function");
+    console.log(id);
+    const user = await User.findById(id);
+    console.log(user);
+    done(null, user);
+  });
+};
